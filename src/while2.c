6c02e09bfa2b7c8b76321d702cf0766de1db698d
Thayna Pontes <thayna01471@gmail.com>
2024-11-01 14:32:48 -0300

    atualizaçao de arquivo

---
+++ src/while2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int s =0;
+    while (s != 20) {
+        printf("Digite um número\n");
+        scanf("%d" ,&s);
+    }
+    
+
+return 0;
+}
---
+++ src/while3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n, i, rs;
+    i = 1;
+    printf ("Digite um número para a tabuada e tecle ENter\n");
+    scanf ("%d" ,&n);
+    while (i <=10) {
+        rs = n*i;
+        printf ("%d x %d = %d\n" ,n,i,rs);
+        i++;
+    }
+    
+
+return 0;
+    }
